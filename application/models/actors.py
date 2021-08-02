a2c1529b00641d73fe5377a410a1f76e03988d8f
boje <jbondy@icloud.com>
2021-08-02 16:39:11 +0200

    WIP models

--- application/models/actors.py
+++ application/models/actors.py
@@ -1,26 +1,27 @@
-from application import db
+from application import db, format_datetime
 from flask import jsonify
+import datetime
 
 
-class Artist(db.Model):
-    __tablename__ = 'actor'
+class Actor(db.Model):
+    __tablename__ = 'actors'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     gender = db.Column(db.String(120))
-    age = db.Column(db.String(120))
+    age = db.Column(db.DateTime(timezone=False), nullable=False)
     phone = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     imdb_link = db.Column(db.String(120))
     website = db.Column(db.String(120))
-    movies = db.relationship('Show', backref='guest_actor', lazy=True, cascade="all, delete-orphan")
+    appearance = db.relationship('Cast', backref='cast_actor', lazy=True, cascade="all, delete-orphan")
 
     def data(self):
         data = {
             "id": self.id,
             "name": self.name,
             "gender": self.gender,
-            "age": self.age,
+            "age": format_datetime(str(self.start_time)),
             "phone": self.phone,
             "website": self.website,
             "imdb_link": self.imdb_link,
