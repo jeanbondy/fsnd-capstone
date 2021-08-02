7c801b9ba380d752ed3d4e5f17d64084618fc1a5
boje <jbondy@icloud.com>
2021-08-02 16:41:30 +0200

    added config and ENV

--- config.py
+++ config.py
@@ -1,13 +1,30 @@
+from dotenv import load_dotenv
 import os
-SECRET_KEY = os.urandom(32)
+
+load_dotenv()
+ENV = os.environ.get('ENV')
+
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Enable debug mode.
-DEBUG = True
+
 
 # Connect to the database
 
+class Config:
+    # Flask
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SECRET_KEY = ('SECRET_KEY')
+    DEBUG = False
 
-# TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres@localhost:5432/fyyur'
+class ProductionConfig(Config):
+    pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
