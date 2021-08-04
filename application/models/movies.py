63a1f95aba86a4fa6c818abbb2c3f60aa47ade27
Jean <2736366-boje@users.noreply.gitlab.com>
2021-08-04 17:11:48 +0000

    endpoints actor movies

--- application/models/movies.py
+++ application/models/movies.py
@@ -9,17 +9,32 @@ class Movie(db.Model):
     release_date = db.Column(db.DateTime(timezone=False), nullable=False)
     image_link = db.Column(db.String(500))
     imdb_link = db.Column(db.String(120))
-    actors = db.relationship('Cast', backref='cast_movie', lazy=True, cascade="all, delete-orphan")
 
-    def data(self):
-        data = {
+    def __init__(self, title, release_date, imdb_link, image_link):
+        self.title = title,
+        self.release_date = release_date,
+        self.imdb_link = imdb_link,
+        self.image_link = image_link
+
+    def format(self):
+        return {
             "id": self.id,
             "title": self.title,
-            "release_date": format_datetime(str(self.start_time)),
+            "release_date": format_datetime(str(self.release_date)),
             "imdb_link": self.imdb_link,
             "image_link": self.image_link
         }
-        return data
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
 
     def __repr__(self):
         return '<Movie %r>' % self
--- application/movie_bp/routes.py
+++ application/movie_bp/routes.py
@@ -1,88 +1,85 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash, jsonify, abort
 from application.models.actors import Actor
 from application.models.movies import Movie
-from application.templates.forms import *
 from application import db
 import sys
 
-actor_bp = Blueprint('actor_bp', __name__)
+movie_bp = Blueprint('movie_bp', __name__)
 
 
-#  All Actors
+#  All Movies
 #  ----------------------------------------------------------------
-@actor_bp.route('/actors', methods=['GET'])
-def actors():
+@movie_bp.route('/movies', methods=['GET'])
+def movies():
     # pagination
     page = request.args.get('page', 1, type=int)
     start = (page - 1) * 10
     end = start + 10
     # query
-    query = Actor.query.all()
-    actors = [actor.format() for actor in query]
+    query = Movie.query.all()
+    movies = [movie.format() for movie in query]
     # create response
-    if len(actors[start:end]) == 0:
+    if len(movies[start:end]) == 0:
         return abort(404)
     else:
         return jsonify({
             'success': True,
-            'totalActors': len(query),
-            'actors': actors[start:end]}), 200
+            'totalMovies': len(query),
+            'movies': movies[start:end]}), 200
 
 
-#  Search Actor
+#  Search Movie
 #  ----------------------------------------------------------------
-@actor_bp.route('/actors/search', methods=['POST'])
-def search_actor():
+@movie_bp.route('/movies/search', methods=['POST'])
+def search_movie():
     request_body = request.get_json()
     if not request_body:
         abort(400)
     if 'searchTerm' in request_body:
         search_term = (request_body['searchTerm'])
-        query = Actor.query.filter(Actor.name.ilike('%' + search_term + '%')).all()
-        actors = [actor.format() for actor in query]
+        query = Movie.query.filter(Movie.title.ilike('%' + search_term + '%')).all()
+        movies = [movie.format() for movie in query]
         if len(query) == 0:
             abort(404)
         else:
             return jsonify({
                 'success': True,
-                'totalActors': len(query),
-                'actors': actors
+                'totalMovies': len(query),
+                'movies': movies
             }), 200
     else:
         abort(400)
 
 
-#  Actor by ID
+#  Movie by ID
 #  ----------------------------------------------------------------
-@actor_bp.route('/actors/<int:actor_id>', methods=['GET'])
-def actor_by_id(actor_id):
-    actor = Actor.query.filter_by(id=actor_id).one_or_none()
-    if actor is None:
+@movie_bp.route('/movies/<int:movie_id>', methods=['GET'])
+def movie_by_id(movie_id):
+    movie = Movie.query.filter_by(id=movie_id).one_or_none()
+    if movie is None:
         abort(404)
     else:
         return jsonify({
             'success': True,
-            'actors': [actor.format()]}), 200
+            'movies': [movie.format()]}), 200
 
 
-#  Create Actor
+#  Create Movie
 #  ----------------------------------------------------------------
-@actor_bp.route('/actors', methods=['POST'])
-def create_actor():
+@movie_bp.route('/movies', methods=['POST'])
+def create_movie():
     request_body = request.get_json()
     if not request_body:
         abort(400)
     # check if all fields are included in the request
-    if not {'name', 'gender', 'age', 'phone', 'image_link', 'imdb_link'}.issuperset(set(request_body)):
+    if not {'title', 'release_date', 'image_link', 'imdb_link'}.issuperset(set(request_body)):
         abort(400)
-    new_actor = Actor(name=request_body['name'],
-                      gender=request_body['gender'],
-                      age=request_body['age'],
-                      phone=request_body['phone'],
+    new_movie = Movie(title=request_body['title'],
+                      release_date=request_body['release_date'],
                       image_link=request_body['image_link'],
                       imdb_link=request_body['imdb_link'])
     try:
-        new_actor.insert()
+        new_movie.insert()
         return jsonify({'success': True}), 200
     except:
         db.session.rollback()
@@ -91,16 +88,16 @@ def create_actor():
         db.session.close()
 
 
-#  Delete Actor
+#  Delete Movie
 #  ----------------------------------------------------------------
-@actor_bp.route('/actors/<actor_id>', methods=['DELETE'])
-def delete_actor(actor_id):
-    actor = Actor.query.filter_by(id=actor_id).one_or_none()
-    if actor is None:
+@movie_bp.route('/movies/<movie_id>', methods=['DELETE'])
+def delete_movie(movie_id):
+    movie = Movie.query.filter_by(id=movie_id).one_or_none()
+    if movie is None:
         abort(404)
     else:
         try:
-            actor.delete()
+            movie.delete()
             return jsonify({'success': True}), 200
         except:
             db.session.rollback()
@@ -111,29 +108,27 @@ def delete_actor(actor_id):
 
 #  Update
 #  ----------------------------------------------------------------
-@actor_bp.route('/actors/<int:actor_id>/edit', methods=['PATCH'])
-def edit_actor(actor_id):
+@movie_bp.route('/movies/<int:movie_id>/edit', methods=['PATCH'])
+def edit_movie(movie_id):
 
     request_body = request.get_json()
     if not request_body:
         abort(400)
-    actor = Actor.query.filter_by(id=actor_id).one_or_none()
-    if actor is None:
+    edited_movie = Movie.query.filter_by(id=movie_id).one_or_none()
+    if edited_movie is None:
         abort(404)
 
     # check if all fields are included in the request
-    if not {'name', 'gender', 'age', 'phone', 'image_link', 'imdb_link'}.issuperset(set(request_body)):
+    if not {'title', 'release_date', 'image_link', 'imdb_link'}.issuperset(set(request_body)):
         abort(400)
-    actor.name = request_body['name']
-    actor.gender = request_body['gender']
-    actor.age = request_body['age']
-    actor.phone = request_body['phone']
-    actor.image_link = request_body['image_link']
-    actor.imdb_link = request_body['imdb_link']
+    edited_movie.title = request_body['title']
+    edited_movie.release_date = request_body['release_date']
+    edited_movie.image_link = request_body['image_link']
+    edited_movie.imdb_link = request_body['imdb_link']
     try:
-        actor.update()
+        edited_movie.update()
         return jsonify({'success': True,
-                        'actors': [actor.format()]}), 200
+                        'movies': [edited_movie.format()]}), 200
     except:
         db.session.rollback()
         abort(422)
